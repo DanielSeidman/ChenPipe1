343843901d51695bfe355193aa075832f9eb02ca
danielseidman <architeuthis.boy@gmail.com>
2024-01-19 11:47:45 -0500

    Resource format modification

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -2,9 +2,10 @@
 #SBATCH -J sm
 #SBATCH -o out
 #SBATCH -e err
-#SBATCH -p RM
+#SBATCH -p RM-shared
 #SBATCH -n 1
 #SBATCH -t 00:30:00
+#SBATCH --ntasks-per-node=128
 
 CONDA_BASE=$(conda info --base)
 source $CONDA_BASE/etc/profile.d/conda.sh
--- run_pipeline_update.sh
+++ run_pipeline_update.sh
@@ -4,8 +4,7 @@
 #SBATCH -e err
 #SBATCH -p shared
 #SBATCH -n 1
-#SBATCH -t 9000
-#SBATCH --mem=10000
+#SBATCH -t 2:00:00
 
 CONDA_BASE=$(conda info --base)
 source $CONDA_BASE/etc/profile.d/conda.sh
