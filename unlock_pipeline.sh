022199afb03aab54bf588b2b1467bb8fccbe97af
Daniel Seidman <dseidman@br012.ib.bridges2.psc.edu>
2024-03-14 00:49:16 -0400

    temporary point for specific cluster info

---
+++ unlock_pipeline.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -J sm
+#SBATCH -o out
+#SBATCH -e err
+#SBATCH -p RM-shared
+#SBATCH -n 1
+#SBATCH -t 48:00:00
+
+module load anaconda3
+CONDA_BASE=$(conda info --base)
+source $CONDA_BASE/etc/profile.d/conda.sh
+conda activate innersnp
+umask u=rwx,g=rwx,o=rx
+snakemake --snakefile workflow/Snakefile --profile ./profiles/slurm --unlock
+
